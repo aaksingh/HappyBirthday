e7cd2eae5e3f296786267f5503eb4b7970030db2
aaksingh <deathakanote@gmail.com>
2017-06-16 00:47:53 -0700

    First commit

---
+++ app/src/main/java/com/example/ne/happybirthday/happybirthday.java
@@ -0,0 +1,13 @@
+package com.example.ne.happybirthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class happybirthday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_happybirthday);
+    }
+}
